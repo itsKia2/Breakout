3c002e23287ad44d3d053811a335dc269641c722
itsKia2 <omerkarimi29@gmail.com>
2024-12-03 00:58:44 -0600

    modularized big chunks in code

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <cstdio>
 #include <ctime>
 
+void gameOverText(sf::Text &gameOver, sf::Font &font);
+std::vector<sf::RectangleShape> initBoxes(std::vector<sf::RectangleShape> &boxes, int boxNum, sf::Vector2f boxSize, sf::FloatRect circleBounds);
+void moveCircle(sf::CircleShape &circle, sf::Vector2u windowSize);
 bool isOverlapping(const sf::FloatRect &rect1, const sf::FloatRect &rect2);
 
 int main(int argc, char *argv[]) {
@@ -26,16 +29,63 @@ int main(int argc, char *argv[]) {
 	std::vector<sf::RectangleShape> boxes;
 	int boxCount = 10;
 	sf::Vector2f boxSize(100, 100);
-	// Random position of box
-	std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+	// Initialize all the boxes
+	initBoxes(boxes, 10, boxSize, circleBounds);
 
 	// Create game over text
-	sf::Text gameOverText;
+	sf::Text gameOver;
 	sf::Font font;
 	if (!font.loadFromFile("/usr/share/fonts/noto/NotoSans-Bold.ttf")) {
 		// Error handling if font doesn't load
-		return -1;
+		printf("error for font\n");
 	}
+	gameOver.setPosition(sf::Vector2f(
+		window.getSize().x / 2.0f, window.getSize().y / 2.0f));
+	gameOverText(gameOver, font);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		// Movement logic with collision detection
+		sf::FloatRect circleBounds = circle.getGlobalBounds();
+
+		// Keyboard input which moves the circle
+		moveCircle(circle, window.getSize());
+
+		window.clear();
+		if (gameChecker) {
+			window.draw(gameOver);
+		} else {
+			window.draw(circle);
+			for (const auto &box : boxes) {
+				window.draw(box);  // Draw all boxes
+			}
+		}
+		window.display();
+
+		std::vector<sf::RectangleShape> box2;
+		for (const auto &box : boxes) {
+			if (circleBounds.intersects(box.getGlobalBounds())) {
+				printf("box has been hit!\n");
+			} else {
+				box2.push_back(box);
+			}
+		}
+		if (box2.empty()) {
+			gameChecker = true;
+		}
+		boxes = box2;
+	}
+
+	return 0;
+}
+
+void gameOverText(sf::Text &gameOverText, sf::Font &font) {
 	gameOverText.setFont(font);
 	gameOverText.setString("GAME OVER");
 	gameOverText.setCharacterSize(50);
@@ -46,10 +96,12 @@ int main(int argc, char *argv[]) {
 
 	gameOverText.setOrigin(textBounds.left + textBounds.width / 2.0f,
 						   textBounds.top + textBounds.height / 2.0f);
-	gameOverText.setPosition(sf::Vector2f(
-		window.getSize().x / 2.0f, window.getSize().y / 2.0f));	 // Game Over Text
+}
 
-	while (boxes.size() < boxCount) {
+std::vector<sf::RectangleShape> initBoxes(std::vector<sf::RectangleShape> &boxes, int boxNum, sf::Vector2f boxSize, sf::FloatRect circleBounds) {
+	// Random position of box
+	std::srand(static_cast<unsigned int>(std::time(nullptr)));
+	while (boxes.size() < boxNum) {
 		sf::RectangleShape box(boxSize);
 		box.setFillColor(sf::Color::Red);
 
@@ -74,68 +126,33 @@ int main(int argc, char *argv[]) {
 			boxes.push_back(box);
 		}
 	}
+	return boxes;
+}
 
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
+void moveCircle(sf::CircleShape &circle, sf::Vector2u windowSize) {
+	// Check for keyboard input and move the circle
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		if (circle.getPosition().x > 0) {
+			circle.move(-0.1, 0);
 		}
-		// Movement logic with collision detection
-		sf::FloatRect circleBounds = circle.getGlobalBounds();
-
-		// Check for keyboard input and move the circle
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-			if (circle.getPosition().x > 0) {
-				circle.move(-0.1, 0);
-			}
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-			if (circle.getPosition().x + circle.getRadius() * 2 <
-				window.getSize().x) {
-				circle.move(0.1, 0);
-			}
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-			if (circle.getPosition().y > 0) {
-				circle.move(0, -0.1);
-			}
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-			if (circle.getPosition().y + circle.getRadius() * 2 <
-				window.getSize().y) {
-				circle.move(0, 0.1);
-			}
-		}
-
-		window.clear();
-		if (gameChecker) {
-			window.draw(gameOverText);
-		} else {
-			window.draw(circle);
-			for (const auto &box : boxes) {
-				window.draw(box);  // Draw all boxes
-			}
-		}
-		window.display();
-
-		std::vector<sf::RectangleShape> box2;
-		for (const auto &box : boxes) {
-			if (circleBounds.intersects(box.getGlobalBounds())) {
-				printf("box has been hit!\n");
-			} else {
-				box2.push_back(box);
-			}
-		}
-		if (box2.empty()) {
-			// printf("Game Over!\n");
-			gameChecker = true;
-		}
-		boxes = box2;
 	}
-
-	return 0;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		if (circle.getPosition().x + circle.getRadius() * 2 <
+			windowSize.x) {
+			circle.move(0.1, 0);
+		}
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		if (circle.getPosition().y > 0) {
+			circle.move(0, -0.1);
+		}
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		if (circle.getPosition().y + circle.getRadius() * 2 <
+			windowSize.y) {
+			circle.move(0, 0.1);
+		}
+	}
 }
 
 bool isOverlapping(const sf::FloatRect &rect1, const sf::FloatRect &rect2) {
@@ -148,5 +165,4 @@ bool isOverlapping(const sf::FloatRect &rect1, const sf::FloatRect &rect2) {
 	sf::FloatRect box2(rect2.getPosition().x, rect2.getPosition().y, rect2X,
 					   rect2Y);
 	return box1.intersects(box2);
-	// return rect1.intersects(rect2);
 }
